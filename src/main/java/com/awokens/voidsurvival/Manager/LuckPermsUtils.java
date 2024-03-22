275686ddc84ab49efb4e49a7b1298197b3b9d45a
Awokens <35590883+Awokens@users.noreply.github.com>
2024-03-22 15:19:57 -0700

    Created the LuckPerms utilities class

---
+++ src/main/java/com/awokens/voidsurvival/Manager/LuckPermsUtils.java
@@ -0,0 +1,69 @@
+package com.awokens.voidsurvival.Manager;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.cacheddata.CachedMetaData;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.NodeType;
+import net.luckperms.api.node.types.MetaNode;
+import org.bukkit.entity.Player;
+
+public class LuckPermsUtils {
+
+    private final LuckPerms API;
+
+    public LuckPermsUtils() {
+        API = LuckPermsProvider.get();
+    }
+
+    public boolean hasToggledItems(Player player) {
+        // obtain CachedMetaData - the easiest way is via the PlayerAdapter
+        // of course, you can get it via a User too if the player is offline.
+        CachedMetaData metaData = API.getPlayerAdapter(Player.class).getMetaData(player);
+
+        // query & parse the meta value
+        return metaData.getMetaValue("items", Boolean::parseBoolean).orElse(true);
+    }
+
+    public void setToggledItems(Player player, boolean toggleItems) {
+        // obtain a User instance (by any means! see above for other ways)
+        User user = API.getPlayerAdapter(Player.class).getUser(player);
+
+        // create a new MetaNode holding the level value
+        // of course, this can have context/expiry/etc too!
+        MetaNode node = MetaNode.builder("items", Boolean.toString(toggleItems)).build();
+
+        // clear any existing meta nodes with the same key - we want to override
+        user.data().clear(NodeType.META.predicate(mn -> mn.getMetaKey().equals("items")));
+        // add the new node
+        user.data().add(node);
+
+        // save!
+        API.getUserManager().saveUser(user);
+    }
+
+    public boolean hasBossBarToggled(Player player) {
+        // Obtain CachedMetaData via the LuckPerms API
+        CachedMetaData metaData = API.getPlayerAdapter(Player.class).getMetaData(player);
+
+        // Query & parse the meta value
+        return metaData.getMetaValue("bossbar", Boolean::parseBoolean).orElse(true);
+    }
+
+    public void setBossBarToggled(Player player, boolean toggleBossBar) {
+        // Obtain a User instance via the LuckPerms API
+        User user = API.getPlayerAdapter(Player.class).getUser(player);
+
+        // Create a new MetaNode holding the toggleBossBar value
+        MetaNode node = MetaNode.builder("bossbar", Boolean.toString(toggleBossBar)).build();
+
+        // Clear any existing meta nodes with the same key to override
+        user.data().clear(NodeType.META.predicate(mn -> mn.getMetaKey().equals("bossbar")));
+        // Add the new node
+        user.data().add(node);
+
+        // Save the changes
+        API.getUserManager().saveUser(user);
+    }
+
+}
