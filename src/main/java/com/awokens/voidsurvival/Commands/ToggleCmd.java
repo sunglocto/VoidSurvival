516ef2a7ac4dc457d9ce920536c5bce6caf24f59
Awokens <35590883+Awokens@users.noreply.github.com>
2024-03-22 15:25:08 -0700

    Added /toggle <type> to toggle off or on certain displays such as the bossbar

---
+++ src/main/java/com/awokens/voidsurvival/Commands/ToggleCmd.java
@@ -0,0 +1,56 @@
+package com.awokens.voidsurvival.Commands;
+
+import com.awokens.voidsurvival.VoidSurvival;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.BooleanArgument;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+
+public class ToggleCmd {
+
+    public ToggleCmd() {
+
+        CommandAPICommand items = new CommandAPICommand("items")
+                .executesPlayer((player, args) -> {
+
+                    boolean toggled = VoidSurvival.getLuckPermsUtils().hasToggledItems(player);
+                    toggled = !toggled;
+
+                    VoidSurvival.getLuckPermsUtils().setToggledItems(player, toggled);
+
+                    player.sendMessage(MiniMessage.miniMessage().deserialize(
+                            "Toggled items to <gray>" + toggled
+                    ));
+                });
+
+        CommandAPICommand bossbar = new CommandAPICommand("bossbar")
+                .withFullDescription("Disable the World Map reset bossbar")
+                .executesPlayer((player, args) -> {
+
+                    boolean toggled = VoidSurvival.getLuckPermsUtils().hasBossBarToggled(player);
+
+                    toggled = !toggled;
+
+                    if (!toggled) {
+                        VoidSurvival.getMapResetScheduler().getMapResetBar().removePlayer(player);
+                    } else {
+                        VoidSurvival.getMapResetScheduler().getMapResetBar().addPlayer(player);
+                    }
+
+                    VoidSurvival.getLuckPermsUtils().setBossBarToggled(player, toggled);
+
+                    player.sendMessage(MiniMessage.miniMessage().deserialize(
+                            "Toggled bossbar to <gray>" + toggled
+                    ));
+                });
+
+        new CommandAPICommand("toggle")
+                .withFullDescription("Toggle on or off certain features")
+                .withUsage("/toggle <items, bossbar, stats>")
+                .withSubcommands(items, bossbar)
+                .register();
+
+    }
+}
