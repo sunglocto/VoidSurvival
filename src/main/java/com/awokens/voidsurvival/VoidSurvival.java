c70c520dba6d1f8ce1588c9baadf31151d9b2f38
Awokens <35590883+Awokens@users.noreply.github.com>
2024-03-22 15:26:29 -0700

    Lots of import configuring

--- src/main/java/com/awokens/voidsurvival/VoidSurvival.java
+++ src/main/java/com/awokens/voidsurvival/VoidSurvival.java
@@ -1,13 +1,27 @@
 package com.awokens.voidsurvival;
 
+import com.awokens.voidsurvival.Commands.*;
 import com.awokens.voidsurvival.Listeners.Interact.*;
 import com.awokens.voidsurvival.Listeners.Mobs.*;
 import com.awokens.voidsurvival.Listeners.Player.*;
+import com.awokens.voidsurvival.Manager.LuckPermsUtils;
+import com.awokens.voidsurvival.Manager.RecipeManager;
+import com.awokens.voidsurvival.Manager.WorldResetManager;
+import com.awokens.voidsurvival.Manager.VoidConfigManager;
+import com.samjakob.spigui.SpiGUI;
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIBukkitConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 
 public final class VoidSurvival extends JavaPlugin implements Listener {
@@ -15,14 +29,60 @@ public final class VoidSurvival extends JavaPlugin implements Listener {
 
     private static Plugin plugin;
 
+    private static LuckPermsUtils luckPermsUtils;
+
     public static Plugin getPlugin() {
         return plugin;
+    }
+
+    public static LuckPermsUtils getLuckPermsUtils() { return luckPermsUtils; }
+
+    private static VoidConfigManager voidConfigManager;
+
+    private static WorldResetManager mapResetScheduler;
+
+    private static SpiGUI spiGUI;
+
+    private static RecipeManager recipeManager;
+
+    public static RecipeManager getRecipeManager() { return recipeManager; }
+    public static WorldResetManager getMapResetScheduler() {
+        return mapResetScheduler;
+    }
+
+    public static VoidConfigManager configManager() {
+        return voidConfigManager;
+    }
+
+    public static SpiGUI getSpiGUI() { return spiGUI; }
+
+    @Override
+    public void onLoad() {
+        CommandAPI.onLoad(new CommandAPIBukkitConfig(this).shouldHookPaperReload(true));
+
+        new ToggleCmd();
+        new NonchestCmd();
+        new Respawn();
+        new CommandsCmd();
+        new RecipesCmd();
     }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
+
+        voidConfigManager = new VoidConfigManager(this, getDataFolder());
+
+        luckPermsUtils = new LuckPermsUtils();
+
+        mapResetScheduler = new WorldResetManager();
+
+        spiGUI = new SpiGUI(this);
+
+        recipeManager = new RecipeManager(this);
+
+        CommandAPI.onEnable();
         registerListeners(this, List.of(
                 new Drowned(),
                 new Fishing(),
@@ -41,13 +101,53 @@ public final class VoidSurvival extends JavaPlugin implements Listener {
                 new NautilusTreasure(),
                 new GodVillager(),
                 new Quit(),
-                new Join()
-        ));
+                new Join(),
+                new AsyncChat(),
+                new Bat()));
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+
+                Material[] essentialItems = {
+                        Material.DIRT,
+                        Material.OAK_PLANKS,
+                        Material.COBBLESTONE,
+                        Material.IRON_NUGGET
+                };
+
+                // Creating an instance of Random class
+                Random random = new Random();
+
+                // Generating a random index within the bounds of the array
+                int randomIndex = random.nextInt(essentialItems.length);
+
+                // Getting the random element from the array
+                Material randomElement = essentialItems[randomIndex];
+
+                ItemStack item = new ItemStack(randomElement);
+
+                for (Player player : Bukkit.getOnlinePlayers()) {
+
+                    if (!luckPermsUtils.hasToggledItems(player)) continue;
+
+                    player.getInventory().addItem(item);
+                }
+
+            }
+        }.runTaskTimer(this, 0L, 20L * 10L);
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        CommandAPI.onDisable();
+
+        getMapResetScheduler().getMapResetBar().setVisible(false);
+        getMapResetScheduler().getTask().cancel();
+
+        Bukkit.clearRecipes();
     }
 
     private void registerListeners(Plugin plugin, List<Listener> listeners) {
