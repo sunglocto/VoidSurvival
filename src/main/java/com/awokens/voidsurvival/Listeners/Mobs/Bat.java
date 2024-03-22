4dc1f5b8301389f65e8b75f070a6454657ef1894
Awokens <35590883+Awokens@users.noreply.github.com>
2024-03-22 15:17:12 -0700

    Bats drop leather upon death

---
+++ src/main/java/com/awokens/voidsurvival/Listeners/Mobs/Bat.java
@@ -0,0 +1,17 @@
+package com.awokens.voidsurvival.Listeners.Mobs;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class Bat implements Listener {
+
+    @EventHandler
+    public void death(EntityDeathEvent event) {
+        if (!(event.getEntity() instanceof org.bukkit.entity.Bat)) return;
+
+        event.getDrops().add(new ItemStack(Material.LEATHER));
+    }
+}
