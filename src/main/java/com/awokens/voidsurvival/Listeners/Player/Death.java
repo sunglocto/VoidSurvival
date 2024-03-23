35d8b7ab9263f9f2e61c9176bd09fd866da2cfdd
Awokens <35590883+Awokens@users.noreply.github.com>
2024-03-23 15:56:29 -0700

    Fixed constant phasing through spawn - set velocity to 0 - teleport player asynchronously - added levitation to be extra cautious

--- src/main/java/com/awokens/voidsurvival/Listeners/Player/Death.java
+++ src/main/java/com/awokens/voidsurvival/Listeners/Player/Death.java
@@ -4,6 +4,7 @@ import com.awokens.voidsurvival.Manager.SpawnPointManager;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +12,8 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class Death implements Listener {
 
@@ -31,10 +34,18 @@ public class Death implements Listener {
     public void respawn(PlayerRespawnEvent event) {
         Player player = event.getPlayer();
 
-        player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 5, 1, false, false, false));
-
         if (!event.isBedSpawn()) {
             event.setRespawnLocation(SpawnPointManager.getWorldSpawn());
         }
+
+        Location respawn = event.getRespawnLocation();
+
+        player.setVelocity(new Vector());
+        player.teleportAsync(respawn).thenRun(new BukkitRunnable() {
+            @Override
+            public void run() {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 5, 1, false, false, false));
+            }
+        });
     }
 }
