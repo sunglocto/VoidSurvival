30c21c3edd5c06dcb5ae2b2fbc0912c46a4b26bc
Awokens <35590883+Awokens@users.noreply.github.com>
2024-03-28 23:36:24 -0700

    Simple admin tools

--- src/main/java/com/awokens/voidsurvival/Commands/Admin/InventorySpy.java
+++ src/main/java/com/awokens/voidsurvival/Commands/Admin/InventorySpy.java
@@ -7,31 +7,32 @@ import dev.jorel.commandapi.arguments.ArgumentSuggestions;
 import dev.jorel.commandapi.arguments.PlayerArgument;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 public class InventorySpy {
     public InventorySpy(VoidSurvival plugin) {
+
+        Collection<String> playerNames = new ArrayList<>();
+
+        for (Player player : plugin.getServer().getOnlinePlayers()) {
+            playerNames.add(player.getName());
+        }
+
         CommandAPICommand enderchest = new CommandAPICommand("enderchest")
-                .withArguments(new PlayerArgument("target").replaceSuggestions(ArgumentSuggestions.strings(
-                    plugin.getServer()
-                            .getOnlinePlayers()
-                            .stream()
-                            .map(Player::getName)
-                            .collect(Collectors.joining())
-                ))).executesPlayer((player, args) -> {
+                .withArguments(new PlayerArgument("target").replaceSuggestions(
+                        ArgumentSuggestions.strings(playerNames)))
+                .executesPlayer((player, args) -> {
                     Player target = (Player) args.get(0);
                     if (target == null) return;
                     player.openInventory(target.getEnderChest());
                 });
 
         CommandAPICommand inventory = new CommandAPICommand("inventory")
-                .withArguments(new PlayerArgument("target").replaceSuggestions(ArgumentSuggestions.strings(
-                        plugin.getServer()
-                                .getOnlinePlayers()
-                                .stream()
-                                .map(Player::getName)
-                                .collect(Collectors.joining())
-                ))).executesPlayer((player, args) -> {
+                .withArguments(new PlayerArgument("target").replaceSuggestions(
+                        ArgumentSuggestions.strings(playerNames)))
+                .executesPlayer((player, args) -> {
                     Player target = (Player) args.get(0);
                     if (target == null) return;
                     player.openInventory(target.getInventory());
@@ -42,6 +43,7 @@ public class InventorySpy {
                 .withPermission(CommandPermission.OP)
                 .withSubcommands(enderchest, inventory)
                 .register();
-
     }
+
+
 }
